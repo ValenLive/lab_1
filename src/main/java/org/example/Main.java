3f1c1ee07e5ea5ee6e4e111b2a725cb8784f50ce
ValenLive <phan.star2016@gmail.com>
2023-03-10 00:27:53 +0200

    intial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SDFSFHSAFDASDFHHAFAJFSJFSGLDFSA");
+        System.out.println("Empty project commit");
     }
 }
